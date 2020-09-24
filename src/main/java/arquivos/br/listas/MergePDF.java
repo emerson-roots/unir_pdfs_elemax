e1de7cbde27554318083c6f3262ddaa716e704f6
Emerson <emerson_sardinha@hotmail.com>
2020-09-23 22:09:09 -0300

    Ajustes no pom e delecao dos testes sem uso

--- src/main/java/arquivos/br/listas/MergePDF.java
+++ src/main/java/arquivos/br/listas/MergePDF.java
@@ -11,8 +11,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.swing.JOptionPane;
-
 import com.itextpdf.text.Document;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfContentByte;
